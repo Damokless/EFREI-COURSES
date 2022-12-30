983ec1fb29ff100b2cf32013f85bd79761a8652f
Damokless <damokless@protonmail.com>
2022-12-30 15:51:30 +0100

    add : CODEFLIX_season.02

---
+++ main.js
@@ -0,0 +1,7 @@
+/*const eventbox = require ('./eventbox.js')
+const names = ['Luffy', 'Zoro', 'Usopp', 'Robin', 'Nami', 'Sanji', 'Chopper']
+eventbox.withArgs(names)*/
+
+const streambox = require ('./streambox.js')
+
+streambox.duplicate()
